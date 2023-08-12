a54a334e5c920bf1852d703b73d63c16f442fae9
AngelaNwokoji <141584626+AngelaNwokoji@users.noreply.github.com>
2023-08-12 02:24:48 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+
+const p = document.querySelectorAll('p');
+console.log('p')
+
+
+p.forEach( (p) => {
+    p.style.color = 'blue'
+    p.style.padding ='20px'
+    p.style.fontSize = '40px'
+    p.style.border = '1px solid blue'
+    p.style.margin = '15px 30px'
+} )
+
+const python= document.querySelector('python');
+
